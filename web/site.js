715ab77fbfc9a3386ba6968e4a58b010d0f72baf
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-05-11 21:13:12 +1000

    update cah-all-compact.json, web/index.html and site.js, Not quite sure if this is correct

--- web/site.js
+++ web/site.js
@@ -249,7 +249,7 @@ document.getElementById("download-text").addEventListener(
 );
 
 let deck;
-CAHDeck.fromCompact("https://raw.githubusercontent.com/crhallberg/json-against-humanity/latest/cah-all-compact.json").then((_deck) => {
+CAHDeck.fromCompact("https://raw.githubusercontent.com/FireRat666/json-against-humanity/latest/cah-all-compact.json").then((_deck) => {
   deck = _deck;
   cardCounts(_deck);
   deckCheckboxes(_deck);
